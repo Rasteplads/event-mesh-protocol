5402990f4ebe307d5ea8281ec8e30796e1e9e68c
KiraStaehr <kspe20@student.aau.dk>
2024-04-12 12:55:36 +0200

    spotless

--- src/main/kotlin/rasteplads/messageCache/MessageCache.kt
+++ src/main/kotlin/rasteplads/messageCache/MessageCache.kt
@@ -3,49 +3,45 @@ package org.example.cache
 import java.time.LocalTime
 import java.util.*
 
-
 class MessageCache<T> {
-    val cacheTime : Long = 1
-    private val cacheID : Queue<Pair<T, LocalTime>> = ArrayDeque()
-    fun cacheMessage(msg:T){
-        if (!cacheID.any{ it.first == msg})
+    val cacheTime: Long = 1
+    private val cacheID: Queue<Pair<T, LocalTime>> = ArrayDeque()
+
+    fun cacheMessage(msg: T) {
+        if (!cacheID.any { it.first == msg })
             cacheID.add(Pair(msg, LocalTime.now().plusMinutes(cacheTime)))
 
         checkForOutdatedMessages()
     }
 
     fun checkForOutdatedMessages() {
-        for (msg in cacheID.iterator()){
-            when (msg.second.compareTo(LocalTime.now())){
-                -1 -> { //Message exceeded its cacheTime and needs to be removed
+        for (msg in cacheID.iterator()) {
+            when (msg.second.compareTo(LocalTime.now())) {
+                -1 -> { // Message exceeded its cacheTime and needs to be removed
                     cacheID.remove(msg)
                 }
-                else -> { //The rest of the messages hasn't exceeded its cacheTime
+                else -> { // The rest of the messages hasn't exceeded its cacheTime
                     break
                 }
             }
         }
     }
 
-    fun containsMessage(msg: T): Boolean{
-        return cacheID.any {it.first == msg}
+    fun containsMessage(msg: T): Boolean {
+        return cacheID.any { it.first == msg }
     }
 
-    fun getsize(): Int{
+    fun getsize(): Int {
         return cacheID.size
     }
 
-    fun printAllMessages(){
-        for (msg in cacheID){
+    fun printAllMessages() {
+        for (msg in cacheID) {
             println(msg)
         }
     }
 
-    fun clearCache(){
+    fun clearCache() {
         cacheID.clear()
     }
 }
-
-
-
-
--- src/test/kotlin/rasteplads/messageCacheTest/MessageCacheTest.kt
+++ src/test/kotlin/rasteplads/messageCacheTest/MessageCacheTest.kt
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test
 
 class MessageCacheTest {
     @Test
-    fun testOutdatedStringMessage(){
-        val cache =  MessageCache<String>()
+    fun testOutdatedStringMessage() {
+        val cache = MessageCache<String>()
         cache.cacheMessage("first")
         Thread.sleep(60000L)
         cache.cacheMessage("second")
@@ -15,9 +15,10 @@ class MessageCacheTest {
         assert(cache.containsMessage("second"))
         assert(cache.getsize() == 1)
     }
+
     @Test
-    fun testNoOutdatedStringMessage(){
-        val cache =  MessageCache<String>()
+    fun testNoOutdatedStringMessage() {
+        val cache = MessageCache<String>()
         cache.cacheMessage("first")
         cache.cacheMessage("second")
 
@@ -27,24 +28,26 @@ class MessageCacheTest {
     }
 
     @Test
-    fun testDuplicateStringKey(){
+    fun testDuplicateStringKey() {
         val cache = MessageCache<String>()
-        cache.cacheMessage( "first")
-        cache.cacheMessage( "first")
+        cache.cacheMessage("first")
+        cache.cacheMessage("first")
 
         assert(cache.getsize() == 1)
     }
+
     @Test
-    fun testDuplicateIntKey(){
+    fun testDuplicateIntKey() {
         val cache = MessageCache<Int>()
-        cache.cacheMessage( 1)
-        cache.cacheMessage( 1)
+        cache.cacheMessage(1)
+        cache.cacheMessage(1)
 
         assert(cache.getsize() == 1)
     }
+
     @Test
-    fun testNoOutdatedIntMessage(){
-        val cache =  MessageCache<Int>()
+    fun testNoOutdatedIntMessage() {
+        val cache = MessageCache<Int>()
         cache.cacheMessage(1)
         cache.cacheMessage(2)
 
@@ -52,9 +55,10 @@ class MessageCacheTest {
         assert(cache.containsMessage(2))
         assert(cache.getsize() == 2)
     }
+
     @Test
-    fun testOutdatedIntMessage(){
-        val cache =  MessageCache<Int>()
+    fun testOutdatedIntMessage() {
+        val cache = MessageCache<Int>()
         cache.cacheMessage(1)
         Thread.sleep(60000L)
         cache.cacheMessage(2)
