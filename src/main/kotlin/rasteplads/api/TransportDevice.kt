589514a71bd14efb1f245debf30073a7a20f68a2
Laursen79 <69715502+Laursen79@users.noreply.github.com>
2024-04-17 10:24:21 +0200

    Removed Message and cleaned TransportDevice (#14)
    Fixed TransportDevice belonging to wrong package and naming in EventMeshDevice

--- src/main/kotlin/rasteplads/api/TransportDevice.kt
+++ src/main/kotlin/rasteplads/api/TransportDevice.kt
@@ -1,17 +1,13 @@
 package rasteplads.api
 
-import kotlinx.coroutines.channels.Channel
-
 interface TransportDevice {
-    val receiveChannel: Channel<ByteArray>
-    val transmitChannel: Channel<ByteArray>
     val transmissionInterval: Long
 
-    suspend fun beginTransmitting(message: ByteArray)
+    fun beginTransmitting(message: ByteArray)
 
     fun stopTransmitting()
 
-    suspend fun beginReceiving(callback: suspend (ByteArray) -> Unit)
+    fun beginReceiving(callback: suspend (ByteArray) -> Unit)
 
     fun stopReceiving()
 }
