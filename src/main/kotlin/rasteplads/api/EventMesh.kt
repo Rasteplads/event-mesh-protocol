0d8a4f7594549afe14cfcc3e386f2a9eccd94c64
KiraStaehr <kspe20@student.aau.dk>
2024-04-17 14:57:16 +0200

    added cachetime

--- src/main/kotlin/rasteplads/api/EventMesh.kt
+++ src/main/kotlin/rasteplads/api/EventMesh.kt
@@ -265,7 +265,7 @@ private constructor(
         fun <ID, Data> builder(
             device: Int // TODO: SIMON
         ): Builder<ID, Data> =
-            BuilderImpl(device, MessageCache()) // TODO: REAL TYPE AND INITIALISATION
+            BuilderImpl(device, MessageCache(60)) // TODO: REAL TYPE AND INITIALISATION
 
         /**
          * Creates a [Builder] for [EventMesh] with a provided message cache (set to `null` to
