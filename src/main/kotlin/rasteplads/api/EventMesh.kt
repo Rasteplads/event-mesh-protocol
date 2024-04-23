5ff5107d3f0d574bc1a187c92fb9863989627824
Thomas Lohse <thomas.k.lohse@gmail.com>
2024-04-23 15:44:28 +0200

    mod test

--- src/main/kotlin/rasteplads/api/EventMesh.kt
+++ src/main/kotlin/rasteplads/api/EventMesh.kt
@@ -135,7 +135,7 @@ private constructor(
         btSender
             .compareAndExchange(
                 null,
-                GlobalScope.launch {
+                GlobalScope.launch(Dispatchers.IO) {
                     do {
                         delay(msgSendInterval.toMillis())
                         val id = msgId()
@@ -151,7 +151,7 @@ private constructor(
         btScanner
             .compareAndExchange(
                 null,
-                GlobalScope.launch {
+                GlobalScope.launch(Dispatchers.IO) {
                     do {
                         delay(msgScanInterval.toMillis())
                         device.startReceiving()
