7b6413aa78d8ed4e3e8f96d348639a3dcbfb2906
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2024-05-03 10:31:00 +0200

    Fixed callback not saved

--- src/main/kotlin/rasteplads/api/EventMeshTransmitter.kt
+++ src/main/kotlin/rasteplads/api/EventMeshTransmitter.kt
@@ -6,11 +6,11 @@ class EventMeshTransmitter<TTx>(private val device: TransportDevice<*, TTx>) {
 
     var transmitTimeout: Long = 60000 // 60 sec // TODO: Default val
 
-    fun transmit(message: ByteArray): Unit = runBlocking {
+    fun transmit(message: ByteArray) {
         var cb: TTx? = null
         try {
             cb = device.beginTransmitting(message)
-            delay(transmitTimeout)
+            Thread.sleep(transmitTimeout)
         } finally {
             if (cb != null)
                 device.stopTransmitting(cb)
